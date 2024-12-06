b796e6695547354e017c5755df78cacd96796136
Farid Seifi <farid.seifi@gmail.com>
2024-12-06 12:38:47 -0500

    updating dependencies and adding procfile for heroku

--- demo/sdk_demo/sdk_demo/app.py
+++ demo/sdk_demo/sdk_demo/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 
 from pregnancy_healthy_weight_gain import pregnancy_healthy_weight_gain
-#from prevent_obesity_morbidity_mortality import prevent_obesity_morbidity_mortality
+from prevent_obesity_morbidity_mortality import prevent_obesity_morbidity_mortality
 from uspstf_knowledgebase import USPSTF_KnowledgeBase
 
 app = Flask(__name__)
@@ -21,7 +21,7 @@ def get_result():
     result = USPSTF_KnowledgeBase.calculate_for_all(patient_data)
     
     result["pregnancy_healthy_weight_gain"]=pregnancy_healthy_weight_gain.get_pregnancy_healthy_weight_gain_recommendation(pregnant=patient_data["pregnant"])
-    #result["prevent_obesity_morbidity_mortality"]=prevent_obesity_morbidity_mortality.get_obesity_recommendation(age=patient_data["age"],bmi=patient_data["bmi"])
+    result["prevent_obesity_morbidity_mortality"]=prevent_obesity_morbidity_mortality.get_obesity_recommendation(age=patient_data["age"],bmi=patient_data["bmi"])
  
     return jsonify(result)
 
